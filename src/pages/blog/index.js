3debb6a6698562f8fdb92e5346387880d900e104
vaneo0814 <vorellana814@gmail.com>
2021-08-20 23:33:30 -0400

    have to fix image bug with src issue

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -2,33 +2,35 @@ import Head from 'next/head';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 import { getPostList } from '../../utils/posts';
+import PostList from '../../components/PostList';
 
-  const Blog = ({ postList }) => {
+
+const Blog = ({ postList }) => {
     return (
-      <>
-        <Head>
-          <title>Blog // UP Boutique</title>
-        </Head>
-        <div className='page-wrapper'>
-          <Header />
-          <main>
-            <pre>{JSON.stringify(postList, null, 2)}</pre>
-          </main>
-          <Footer />
-        </div>
-      </>
+        <>
+            <Head>
+                <title>Blog // UP Boutique</title>
+            </Head>
+            <div className='page-wrapper'>
+                <Header />
+                <main>
+                    <PostList posts={postList} />
+                </main>
+                <Footer />
+            </div>
+        </>
     );
-  };
-  
-  //getStaticProps() function here
-  export const getStaticProps = () => {
+};
+
+//getStaticProps() function here
+export const getStaticProps = () => {
     const postList = getPostList();
     return {
-      props: {
-        postList,
-      },
+        props: {
+            postList,
+        },
     };
-  };
+};
 
-  
-  export default Blog;
+
+export default Blog;
